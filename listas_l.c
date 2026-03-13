8599a27d2247c280bc615ad5910b9e0244750785
Sonia <sonia@midominio.local>
2026-03-13 11:46:54 -0600

    se agrega sacar_frente, buscar_sacar, matar

--- listas_l.c
+++ listas_l.c
@@ -6,14 +6,15 @@ typedef struct PCB {
     int PID;
     char nombre_proceso[50];
     int PC;
+    int estado;
     char IR[64];
     int EAX,EBX,ECX,EDX;
     struct PCB *sig;
 }PCB;
 
-PCB crear_nodo(int pid, char nombre_proceso[], int PC, char IR[], int EAX, int EBX, int ECX, int EDX){
+PCB *crear_nodo(int pid, char nombre_proceso[], int PC, char IR[], int EAX, int EBX, int ECX, int EDX){
 
-    PCB *nuevo = malloc(sizeof(struct PCB));
+    PCB *nuevo = malloc(sizeof(PCB));
     nuevo->PID = pid;
     strcpy(nuevo->nombre_proceso, nombre_proceso); //Cambiar por strncpy
     nuevo->PC = PC;
@@ -24,15 +25,10 @@ PCB crear_nodo(int pid, char nombre_proceso[], int PC, char IR[], int EAX, int E
     nuevo->EDX = EDX;
     
     nuevo->sig = NULL; //el sig del nodo nuevo debe apuntar a nulo porque lo vamos a insertar al final
-    //return nuevo;
+    return nuevo;
 }
 
 void insertar(PCB *lista, PCB *nuevo){
-
-    /*if(*lista == NULL){
-        *lista = nuevo; //la cabeza debe apuntar al nodo nuevo unicamente si la lista estaba vacia.
-        return;
-    }*/
     
     PCB *temp = lista; //para insertar otro nodo creamos un temp que igualmente apunte a la cabeza de la lista
     
@@ -44,37 +40,100 @@ void insertar(PCB *lista, PCB *nuevo){
 }
 
 void imprimir(PCB *lista){
-
-    //if(lista != NULL){
-
-        PCB *temp = lista; //para imprimir nodos creamos un temp que apunte a la cabeza de la lista
-        
-        while(1){ //recorremos la lista hasta que el sig de algun nodo sea igual a NULL
-            printf("%d\t%s\t%d\t%s\t%d\t%d\t%d\t%d\n",temp->PID,temp->nombre_proceso, temp->PC, temp->IR, temp->EAX, temp->EBX, temp->ECX, temp->EDX);
-            if(temp->sig == NULL){
-                break;
-            }
-            temp = temp->sig;
-        }
-        
-    //}
+    PCB *temp = lista; //para imprimir nodos creamos un temp que apunte a la cabeza de la lista
+    while(temp->sig != NULL){ //recorremos la lista hasta que el sig de algun nodo sea igual a NULL
+        temp = temp->sig;
+        printf("%d\t%s\t%d\t%d\t%s\t%d\t%d\t%d\t%d\n",temp->PID,temp->nombre_proceso, temp->PC, temp->estado, temp->IR, temp->EAX, temp->EBX, temp->ECX, temp->EDX);
+    }
 }
+
+PCB *sacarFrente(PCB *lista){  
+    
+    if(lista->sig == NULL){
+        return NULL;
+    }
+
+    PCB *temp = lista->sig;
+    lista->sig = temp->sig;
+    temp->sig=NULL;
+    return temp;
+}   
+
+PCB *buscar_sacar(PCB *lista, int num_PID){  
+    
+    if(lista->sig == NULL){
+        return NULL;
+    }
+
+    PCB *temp1 = lista;
+    PCB *temp2 = lista->sig;
+
+    while(temp1->sig != NULL){
+        if(temp2->PID == num_PID){
+            temp1->sig = temp2->sig;
+            temp2->sig = NULL;
+            return temp2;
+        }
+        else{
+            temp1= temp1->sig;
+            temp2= temp2->sig;
+        }
+    }
+    return NULL;
+}  
+
+void matar(PCB *lista_listos, PCB *lista_ejecucion, PCB *lista_terminados, int num_PID){
+    PCB *matar;
+    if((matar = buscar_sacar(lista_listos, num_PID)) != NULL){
+        insertar(lista_terminados, matar);
+    }
+    else if((matar = buscar_sacar(lista_ejecucion, num_PID)) != NULL){
+        insertar(lista_terminados, matar);
+    }
+    else if((matar = buscar_sacar(lista_terminados, num_PID)) != NULL){
+        printf("\nEste proceso ya esta terminado\n");
+    }
+    else{
+        printf("\nNo existe ese proceso\n");
+    }
+}
+
 int main() {
     char nombre_proceso[50];
-    PCB listos; 
+    PCB listos; //cabeza
     PCB ejecucion;
     PCB terminados;
+    int num_PID = 5;
+
+    listos.sig = NULL;
+    ejecucion.sig = NULL;
+    terminados.sig = NULL;
 
     //FILE *arc_instrucciones = fopen("hola.txt","r");
-
-    for(int i = 0; i < 6 ; i++){
+    for(int i = 1; i <= 4 ; i++){
         printf("Ingrese el nombre del proceso: \n");
         scanf("%s", nombre_proceso);
-        PCB nuevo = crear_nodo(i,nombre_proceso,0,0,0,0,0,0);
-        insertar(&listos, &nuevo);
-        imprimir(&listos);
+        PCB *nuevo = crear_nodo(i,nombre_proceso,0,"MOV",0,0,0,0);
+        insertar(&listos, nuevo);
     }
+    imprimir(&listos);
+
+    PCB *meterEjecucion = sacarFrente(&listos);  
+    insertar(&ejecucion, meterEjecucion);
+    printf("\n");
+    imprimir(&listos);
+    printf("\n");
+    imprimir(&ejecucion);
+
+    matar(&listos, &ejecucion, &terminados, num_PID);
+
+    printf("\n");
+    imprimir(&listos);
+    printf("\n");
+    imprimir(&ejecucion);
+    printf("\n");
+    imprimir(&terminados);
 
     //imprimir(&listos);
     return 0;
-}
+}        
